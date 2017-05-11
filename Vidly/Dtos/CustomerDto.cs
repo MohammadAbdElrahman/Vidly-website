b2c804b9ff42cfad4dd7c22d5974cc27010d839f
Mohamad Abdalrahman <muhamad.abdalrahman@gmail.com>
2017-05-11 18:15:50 +0200

    Build an API

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "please Enter the Customer Name ")]
+
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        
+        public byte MembershipTypeId { get; set; }
+
+
+        //[Min18]
+        public DateTime? BirthDate { get; set; }
+    }
+}
